4519879f3fbd7fabe8f24a2fac0717bcb2a8d621
sukkla01 <sukkla01@gmail.com>
2019-11-29 16:22:58 +0700

    3

--- config.js
+++ config.js
@@ -1,10 +1,20 @@
 module.exports = {
     secret: '1234',
     dbOption: {
-        host: 'localhost',
-        user: 'sukkla01',
+        host: '192.168.1.113',
+        user: 'root',
         password: '0810432245',
         database: 'scan_opd',
         port: '3306'
     }
 }
+// module.exports = {
+//     secret: '1234',
+//     dbOption: {
+//         host: 'localhost',
+//         user: 'sukkla01',
+//         password: '0810432245',
+//         database: 'scan_opd',
+//         port: '3306'
+//     }
+// }
