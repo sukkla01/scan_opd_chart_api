e5995b1d0e5143f9d21f7d06b9b5652065b00bc8
sukkla01 <sukkla01@gmail.com>
2019-11-30 09:23:51 +0700

    12

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     secret: '1234',
     dbOption: {
-        host: '192.168.1.113',
-        user: 'root',
-        password: '0810432245',
+        host: '192.168.1.13',
+        user: 'admin',
+        password: 'ictsrisangworn',
         database: 'scan_opd',
         port: '3306'
     }
