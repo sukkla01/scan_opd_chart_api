ca1058affc51a20ef1a83ceaf9b9d141fa0f676b
sukkla01 <sukkla01@gmail.com>
2019-11-30 10:26:39 +0700

    22

--- controllers/Patient/Index.js
+++ controllers/Patient/Index.js
@@ -7,6 +7,7 @@ exports.findPatient = (req, res, next) => {
         // var params = "%" + req.query.term + "%"
         connection.query(sql, (err, results) => {
             if (err) return next(err)
+            res.header("Content-Type", "application/json; charset=utf-8");
             res.send(results)
         })
     })
